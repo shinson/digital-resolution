7a75acefbeab56c1726bb240bbd18debd3e6cefa
shinson <sonia.hinson@gmail.com>
2015-02-28 15:00:06 -0500

    Change colors, photos, structure, images to grumpy cat, refresh time to 3 secs

--- settings.py
+++ settings.py
@@ -59,13 +59,9 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'digitalresolution',
-        'USER': 'digitalresolution',
-        'PASSWORD': 'digitalresolution',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
 
 TEMPLATE_DIRS = (
